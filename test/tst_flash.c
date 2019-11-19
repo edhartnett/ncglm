169270ca620b03c6500cf5b10a07ae71e7833f1c
Edward Hartnett <edwardjameshartnett@gmail.com>
2019-11-19 05:07:18 -0700

    starting to add flash and group tests

---
+++ test/tst_flash.c
@@ -0,0 +1,53 @@
+/*
+  Program to read flash data from the GOES-17 Global Lightning Mapper.
+
+  Ed Hartnett, 11/19/19
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include "un_test.h"
+#include "goes_glm.h"
+
+/* Err is used to keep track of errors within each set of tests,
+ * total_err is the number of errors in the entire test program, which
+ * generally cosists of several sets of tests. */
+int total_err = 0, err = 0;
+
+int
+main()
+{
+    printf("Testing GLM flash reads.\n");
+    printf("testing GLM flash reads...");
+    {
+        int ncid;
+        size_t nevent, ngroup, nflash;
+        GLM_FLASH_T *flash;
+        int ret;
+
+        /* Open the data file as read-only. */
+        if ((ret = nc_open(GLM_DATA_FILE, NC_NOWRITE, &ncid)))
+            NC_ERR(ret);
+
+        /* Check number of flashs, groups, and flashes. */
+        if ((ret = glm_read_dims(ncid, &nevent, &ngroup, &nflash))) ERR;
+        if (nevent != 4578 || ngroup != 1609 || nflash != 123) ERR;
+
+        /* Allocat array of struct to hold data. */
+        if (!(flash = malloc(nflash * sizeof(GLM_FLASH_T)))) ERR;
+
+        /* Read data. */
+        if (glm_read_flash_structs(ncid, nflash, flash)) ERR;
+
+        /* Free resources. */
+        free(flash);
+
+        /* Close the data file. */
+        if ((ret = nc_close(ncid)))
+            NC_ERR(ret);
+    }
+   SUMMARIZE_ERR;
+   FINAL_RESULTS;
+}
---
+++ test/tst_group.c
@@ -0,0 +1,53 @@
+/*
+  Program to read group data from the GOES-17 Global Lightning Mapper.
+
+  Ed Hartnett, 11/19/19
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include "un_test.h"
+#include "goes_glm.h"
+
+/* Err is used to keep track of errors within each set of tests,
+ * total_err is the number of errors in the entire test program, which
+ * generally cosists of several sets of tests. */
+int total_err = 0, err = 0;
+
+int
+main()
+{
+    printf("Testing GLM group reads.\n");
+    printf("testing GLM group reads...");
+    {
+        int ncid;
+        size_t nevent, ngroup, nflash;
+        GLM_GROUP_T *group;
+        int ret;
+
+        /* Open the data file as read-only. */
+        if ((ret = nc_open(GLM_DATA_FILE, NC_NOWRITE, &ncid)))
+            NC_ERR(ret);
+
+        /* Check number of events, groups, and flashes. */
+        if ((ret = glm_read_dims(ncid, &nevent, &ngroup, &nflash))) ERR;
+        if (nevent != 4578 || ngroup != 1609 || nflash != 123) ERR;
+
+        /* Allocat array of struct to hold data. */
+        if (!(group = malloc(ngroup * sizeof(GLM_GROUP_T)))) ERR;
+
+        /* Read data. */
+        if (glm_read_group_structs(ncid, ngroup, group)) ERR;
+
+        /* Free resources. */
+        free(group);
+
+        /* Close the data file. */
+        if ((ret = nc_close(ncid)))
+            NC_ERR(ret);
+    }
+   SUMMARIZE_ERR;
+   FINAL_RESULTS;
+}
