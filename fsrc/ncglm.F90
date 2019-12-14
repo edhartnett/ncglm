8e6830b1850d57b00ddbd6df2fa77073f8fc7cc9
Edward Hartnett <edwardjameshartnett@gmail.com>
2019-12-14 09:13:06 -0700

    adding the fortran wrappers

---
+++ fsrc/ncglm.F90
@@ -0,0 +1,7 @@
+  ! This is the Fortran wrapper for the ncglm library.
+
+  ! Ed Hartnett 12/14/19
+
+module ncglm
+
+end module ncglm
---
+++ ftest/ftst_glm_read.F90
@@ -0,0 +1,7 @@
+  ! This is a test program for the ncglm fortran wrappers.
+
+  ! Ed Hartnett 12/14/19
+
+program ftst_glm_read
+  print *, 'howdy'
+end program ftst_glm_read
