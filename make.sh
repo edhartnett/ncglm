9cd72c59ccc15fef254f43416323fdc53cab0502
TheTrueProblematic <mwmttl@gmail.com>
2021-02-14 03:52:27 -0700

    Moved make.sh

---
+++ make.sh
@@ -0,0 +1,4 @@
+make -f psrc/Makefiles/event_Makefile
+make -f psrc/Makefiles/flash_Makefile
+make -f psrc/Makefiles/group_Makefile
+make -f psrc/Makefiles/read_Makefile
--- psrc/make.sh
+++
@@ -1,4 +0,0 @@
-make -f Makefiles/event_Makefile
-make -f Makefiles/flash_Makefile
-make -f Makefiles/group_Makefile
-make -f Makefiles/read_Makefile
