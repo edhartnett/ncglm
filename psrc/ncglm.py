1596e0d3865481fbb3413615a102ba0d151318cc
TheTrueProblematic <mwmttl@gmail.com>
2021-03-16 16:17:25 -0600

    fix to use the libncglm.so file that still needs to be made from libncglm.la at this point

--- psrc/ncglm.py
+++ psrc/ncglm.py
@@ -3,30 +3,44 @@
 # Maximilian McClelland 1/29/21
 
 import ctypes
+import os
+from termcolor import colored
+
+temp = os.path.abspath(__file__)
+temp = os.path.realpath(temp)
+temp = os.path.dirname(temp)
+end = len(temp) - 4
+path = temp[:end] + "src"
+lib = os.path.join(path, "libncglm.so")
+
+path = "\nPath: \n" + lib + "\n"
+print(colored(path, 'green'))
 
 # Creating Libraries
-glm_group = ctypes.CDLL("../src/glm_group.so")
-glm_event = ctypes.CDLL("../src/glm_event.so")
-glm_read = ctypes.CDLL("../src/glmread_.so")
-glm_flash = ctypes.CDLL("../src/glm_flash.so")
+try:
+    lib_ncglm = ctypes.CDLL(lib)
+except Exception as err:
+    err = "Could not load NCGLM Library:\n" + str(err)
+    print(colored(err, 'red'))
+    exit()
 
 # Specifying Functions
-read_group_vars = glm_group.read_group_vars
-glm_read_group_structs = glm_group.glm_read_group_structs
-glm_read_group_arrays = glm_group.glm_read_group_arrays
+read_group_vars = lib_ncglm.read_group_vars
+glm_read_group_structs = lib_ncglm.glm_read_group_structs
+glm_read_group_arrays = lib_ncglm.glm_read_group_arrays
 
-read_event_vars = glm_event.read_event_vars
-glm_read_event_structs = glm_event.glm_read_event_structs
-glm_read_event_arrays = glm_event.glm_read_event_arrays
+read_event_vars = lib_ncglm.read_event_vars
+glm_read_event_structs = lib_ncglm.glm_read_event_structs
+glm_read_event_arrays = lib_ncglm.glm_read_event_arrays
 
-read_flash_vars = glm_flash.read_flash_vars
-glm_read_flash_structs = glm_flash.glm_read_flash_structs
-glm_read_flash_arrays = glm_flash.glm_read_flash_arrays
+read_flash_vars = lib_ncglm.read_flash_vars
+glm_read_flash_structs = lib_ncglm.glm_read_flash_structs
+glm_read_flash_arrays = lib_ncglm.glm_read_flash_arrays
 
-glm_read_dims = glm_read.glm_read_dims
-read_scalars = glm_read.read_scalars
-glm_read_file = glm_read.glm_read_file
-glm_read_file_arrays = glm_read.glm_read_file_arrays
+glm_read_dims = lib_ncglm.glm_read_dims
+read_scalars = lib_ncglm.read_scalars
+glm_read_file = lib_ncglm.glm_read_file
+glm_read_file_arrays = lib_ncglm.glm_read_file_arrays
 
 # Specify inputs
 # read_group_vars.argtypes = [ctypes.c_int, ctypes.]
