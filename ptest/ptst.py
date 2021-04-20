0903c73f236cdae4986a319280a484540f34f484
TheTrueProblematic <mwmttl@gmail.com>
2021-04-20 15:51:03 -0600

    Just some color infrastructure for later

--- ptest/ptst.py
+++ ptest/ptst.py
@@ -10,6 +10,16 @@ EPSILON = 0.0001
 NUM_VAL = 5
 NC_MAX_INT = 2147483647
 
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
 def main():
     print("TestingPythonWrapper\n")
